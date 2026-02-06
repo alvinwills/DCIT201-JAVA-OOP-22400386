77abd252868ddab2c1f13e383083b5e40c4060d7
Alvinwills <abiawilliamsa@gmail.com>
2026-02-06 08:51:49 +0000

    Implementing method called calculate average with takes two arguments

--- question2.java
+++ question2.java
@@ -1,5 +1,8 @@
 // Going to show method overloading in this example
 
 public class Question2{
-    
+     public static double calculateClassAverage(double score1, double score2) {
+        return (score1 + score2) / 2.0;
+    }
+
 }
