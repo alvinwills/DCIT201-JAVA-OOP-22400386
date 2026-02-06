2970f3e70c76163c11a5f68ed15116a5448dd119
Alvinwills <abiawilliamsa@gmail.com>
2026-02-06 08:53:41 +0000

    Implementing the third method with the same name but with an array of doubles as arguments

--- question2.java
+++ question2.java
@@ -10,4 +10,13 @@ public class Question2{
         return (score1 + score2 + score3) / 3.0;
     }
 
+    public static double calculateClassAverage(double[] scores) {
+        
+        double sum = 0.0;
+        for (double score : scores) {
+            sum += score;
+        }
+        return sum / scores.length;
+    }
+
 }
