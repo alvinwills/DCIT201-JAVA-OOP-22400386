de1227c43a366eca255e55f802f54a4b1e091140
Alvinwills <abiawilliamsa@gmail.com>
2026-02-06 08:46:17 +0000

    Implement function to calculate the letter grade based on score

--- question1.java
+++ question1.java
@@ -2,4 +2,13 @@ public class Question1{
      public static boolean validateScore(double score) {
         return score >= 0 && score <= 100;
     }
+
+
+    public static char calculateLetterGrade(double score) {
+        if (score >= 80) return 'A';
+        else if (score >= 70) return 'B';
+        else if (score >= 60) return 'C';
+        else if (score >= 50) return 'D';
+        else return 'F';
+    }
 }
