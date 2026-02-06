6958e5008fcbbf86d72463f77e52d39613a87346
Alvinwills <abiawilliamsa@gmail.com>
2026-02-06 08:52:25 +0000

    Implementing the same method but with 3 arguments demonstrating method overloading

--- question2.java
+++ question2.java
@@ -1,8 +1,13 @@
 // Going to show method overloading in this example
 
 public class Question2{
+     
      public static double calculateClassAverage(double score1, double score2) {
         return (score1 + score2) / 2.0;
+    }
+
+    public static double calculateClassAverage(double score1, double score2, double score3) {
+        return (score1 + score2 + score3) / 3.0;
     }
 
 }
