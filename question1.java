fd056ba5475a93e9800236b284befba1f6f222e7
Alvinwills <abiawilliamsa@gmail.com>
2026-02-06 08:47:22 +0000

    Implement displayperformance message to show a remark message based on grade

--- question1.java
+++ question1.java
@@ -11,4 +11,26 @@ public class Question1{
         else if (score >= 50) return 'D';
         else return 'F';
     }
+
+     public static void displayPerformanceMessage(char grade) {
+        switch (grade) {
+            case 'A':
+                System.out.println("Excellent work!");
+                break;
+            case 'B':
+                System.out.println("Very good!");
+                break;
+            case 'C':
+                System.out.println("Good effort.");
+                break;
+            case 'D':
+                System.out.println("You passed.");
+                break;
+            case 'F':
+                System.out.println("Needs improvement.");
+                break;
+            default:
+                System.out.println("Unknown grade.");
+        }
+    }
 }
